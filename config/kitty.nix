567ed1eab75bb225a9356b659495f6b81d1f26be
exform <mmillwood256@gmail.com>
2023-03-20 13:12:13 -0500

    boop

--- config/kitty.nix
+++ config/kitty.nix
@@ -5,7 +5,7 @@
     # "ctrl+z" = "signal_child SIGTERM"; # this is a shell thing (used zsh for it)
   };
   settings = {
-    font_size = "24.0";
+    font_size = "22.0";
     font_family = a.style.mono-font;
     bold_font = "auto";
     italic_font = "auto";
