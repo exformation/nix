155c9c9ec5fddef0a4bca63cdab9e72cf17eccd7
exform <mmillwood256@gmail.com>
2023-06-13 17:55:50 -0500

    boop

--- config/home/programs/kitty.nix
+++ config/home/programs/kitty.nix
@@ -1,3 +1,5 @@
+# TODO: make a bind for easily switching between the windows
+# youu could just override your normal one if you're focused on kitty
 { user, ... }: {
   home-manager.users."${user}".programs.kitty = {
     enable = true;
@@ -13,6 +15,7 @@
       # bold_italic_font = "auto";
       allow_remote_control = true;
       # TODO: create this programmatically for all your tabs by looping through the projects and ~/repos 
+      # hmmm that way if people don't even have a repos folder it can still make a valid config
       startup_session = "startup.conf";
       confirm_os_window_close = "0";
     };
