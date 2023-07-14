e9d8d46e022dfe5b463d7327a1515e4ab568f29a
exform <mmillwood256@gmail.com>
2023-07-14 17:09:12 -0500

    boop

--- config/home/programs/programs.nix
+++ config/home/programs/programs.nix
@@ -1,5 +1,5 @@
 { user, pkgs, ... }: {
-  imports = [ ./git.nix ./zsh.nix ./kitty.nix ./nvim.nix ];
+  imports = [ ./waybar.nix ./git.nix ./zsh.nix ./kitty.nix ./nvim.nix ];
   home-manager.users."${user}".programs = {
     direnv = {
       enable = true;
@@ -37,31 +37,6 @@
     google-chrome.enable = true;
     jq.enable = true;
     lf.enable = true;
-    waybar.enable = true;
-    waybar.settings = {
-      mainBar = {
-        layer = "top";
-        position = "top";
-        height = 30;
-        output = [ "eDP-1" "HDMI-A-1" ];
-        modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
-        modules-center = [ "sway/window" "custom/hello-from-waybar" ];
-        modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
-
-        "sway/workspaces" = {
-          disable-scroll = true;
-          all-outputs = true;
-        };
-        "custom/hello-from-waybar" = {
-          format = "hello {}";
-          max-length = 40;
-          interval = "once";
-          exec = pkgs.writeShellScript "hello-from-waybar" ''
-            echo "from within waybar"
-          '';
-        };
-      };
-    };
     # spotify.enable = true;
     # mako.enable = true;
     # fd.enable = true;
---
+++ config/home/programs/waybar.nix
@@ -0,0 +1,52 @@
+{ user, pkgs, ... }: {
+  home-manager.users."${user}".programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 16;
+        # margin = "20 5";
+        modules-left = [ "hyprland/window" ];
+        modules-center = [ "tray" ];
+        modules-right = [
+          "pulseaudio"
+          "backlight"
+          "temperature"
+          "battery"
+          "clock"
+          "network"
+          "bluetooth"
+        ];
+        "network" = {
+          format = "{ifname}";
+          format-wifi = "{essid} ({signalStrength}%) ";
+          format-ethernet = "{ipaddr}/{cidr} ";
+          format-disconnected = ""; # An empty format will hide the module.
+          tooltip-format = "{ifname} via {gwaddr} ";
+          tooltip-format-wifi = "{essid} ({signalStrength}%) ";
+          tooltip-format-ethernet = "{ifname} ";
+          tooltip-format-disconnected = "Disconnected";
+          max-length = 50;
+          padding = 10;
+        };
+        "clock" = { format-alt = "{:%a, %d. %b  %H:%M}"; };
+        "custom/hello-from-waybar" = {
+          format = "hello {}";
+          max-length = 40;
+          interval = "once";
+          exec = pkgs.writeShellScript "hello-from-waybar" ''
+            echo "from within waybar"
+          '';
+        };
+      };
+    };
+    style = ''
+      * {
+        border: none;
+        border-radius: 0;
+        padding: 10;
+      }
+    '';
+  };
+}
