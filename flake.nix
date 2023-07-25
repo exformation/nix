1399db7af24d4d5bca9e29cb9da844ef0f970e22
exform <mmillwood256@gmail.com>
2023-07-25 14:24:24 -0500

    boop

--- flake.nix
+++ flake.nix
@@ -14,7 +14,8 @@
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
     hyprland = {
-      url = "github:hyprwm/Hyprland?rev=f6b340cc19d2b1fdca0ae2e72a18c87c63893507";
+      # url = "github:hyprwm/Hyprland?rev=f6b340cc19d2b1fdca0ae2e72a18c87c63893507";
+      url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # discord-bot = {
