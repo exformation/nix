d0215ecbafc47c932fe2f1c1453e7b8ef5a50136
exform <mmillwood256@gmail.com>
2023-11-30 07:32:41 -0600

    boop

--- config/home/programs/git.nix
+++ config/home/programs/git.nix
@@ -5,7 +5,7 @@
       userName = user;
       userEmail = email;
       extraConfig = { init.defaultBranch = "main"; };
-      ignores = [ "shell.nix" "flake.nix" ".envrc" ".direnv/" ".vscode/" ];
+      ignores = [ ".envrc" ".direnv/" ".vscode/" ];
       aliases = {
         c = "commit -am";
         s = "status";
