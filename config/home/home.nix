9bf1914d9f86e5fff60f9bcc65588b3388d65cce
exform <mmillwood256@gmail.com>
2024-02-12 10:36:16 -0600

    boop

--- config/home/home.nix
+++ config/home/home.nix
@@ -5,12 +5,6 @@
     users."${user}" = {config,...}: {
       stylix.targets.vim.enable = false;
       home = { stateVersion = version; };
-      xdg.configFile."nvim/lua" = {
-        source = config.lib.file.mkOutOfStoreSymlink nvim/lua;
-      };
-      xdg.configFile."nvim/init.lua" = {
-        source = config.lib.file.mkOutOfStoreSymlink nvim/init.lua;
-      };	
     };
   };
 }
